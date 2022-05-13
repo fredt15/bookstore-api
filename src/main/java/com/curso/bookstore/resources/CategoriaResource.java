852e0106d13dff32f63d1aa4cd0ee809c7475c96
FredPuc <fredtbh15@gmail.com>
2022-05-13 16:15:24 -0300

    Delete para categoria

--- src/main/java/com/curso/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/curso/bookstore/resources/CategoriaResource.java
@@ -43,4 +43,10 @@ public class CategoriaResource {
         Categoria newObj = service.create(id, objDTO);
         return ResponseEntity.ok().body(new CategoriaDTO(newObj));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void>delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/curso/bookstore/service/CategoriaService.java
+++ src/main/java/com/curso/bookstore/service/CategoriaService.java
@@ -35,4 +35,9 @@ public class CategoriaService {
         obj.setDescricao(objDTO.getDescricao());
         return repository.save(obj);
     }
+
+    public void delete(Integer id) {
+        findById(id);
+        repository.deleteById(id);
+    }
 }
