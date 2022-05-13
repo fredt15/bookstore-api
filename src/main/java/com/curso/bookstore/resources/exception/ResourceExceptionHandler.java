f7500dae608cefd0fca93fc3b0e3fe9729480cf9
FredPuc <fredtbh15@gmail.com>
2022-05-13 16:37:41 -0300

    exceção personalizada para Delete para categoria

--- src/main/java/com/curso/bookstore/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/curso/bookstore/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.curso.bookstore.resources.exception;
 
+import com.curso.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.curso.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,13 @@ public class ResourceExceptionHandler {
                 HttpStatus.NOT_FOUND.value(),
                 e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/curso/bookstore/service/CategoriaService.java
+++ src/main/java/com/curso/bookstore/service/CategoriaService.java
@@ -5,9 +5,8 @@ import com.curso.bookstore.dtos.CategoriaDTO;
 import com.curso.bookstore.repositories.CategoriaRepository;
 import com.curso.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
+import org.springframework.dao.DataIntegrityViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,15 +15,17 @@ public class CategoriaService {
 
     @Autowired
     private CategoriaRepository repository;
-    public Categoria findById(Integer id){
+
+    public Categoria findById(Integer id) {
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElseThrow(()-> new ObjectNotFoundException(
-                "Objeto não encontrado! ID: "+ id + ", Tipo: "+ Categoria.class.getName()));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
     }
-    public List<Categoria> finAlld(){
+
+    public List<Categoria> finAlld() {
         return repository.findAll();
     }
-    public Categoria create (Categoria obj) {
+
+    public Categoria create(Categoria obj) {
         obj.setId(null);
         return repository.save(obj);
     }
@@ -38,6 +39,11 @@ public class CategoriaService {
 
     public void delete(Integer id) {
         findById(id);
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new com.curso.bookstore.service.exceptions.DataIntegrityViolationException(
+                    "Categoria não pode ser delateda! Possui Livros associados.");
+        }
     }
 }
